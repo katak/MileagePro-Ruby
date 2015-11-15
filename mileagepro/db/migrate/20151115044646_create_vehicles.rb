f45ce3784f959b2f606d3a1c34e6e4b68774a922
Kris Kata <kata.krzysztof@gmail>
2015-11-14 22:47:38 -0600

    Add vehicle model

---
+++ mileagepro/db/migrate/20151115044646_create_vehicles.rb
@@ -0,0 +1,11 @@
+class CreateVehicles < ActiveRecord::Migration
+  def change
+    create_table :vehicles do |t|
+      t.integer :year
+      t.string :make
+      t.string :model
+
+      t.timestamps null: false
+    end
+  end
+end
